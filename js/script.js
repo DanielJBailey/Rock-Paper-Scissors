4935bdd053c2cac4c9c2d050807cbceebc4daec4
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-02 22:40:42 -0700

    finish game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let playerName;
     let userChoice;
     let compChoice;
     let winner;
+    let playerScore = 0;
+    let compScore = 0;
+    let paperAudio = new Audio('./audio/paperrockscissors.mp3');
+    let loseAudio = new Audio('./audio/lose.mp3');
+    let winAudio = new Audio('./audio/destroy.mp3');
+    let winAudio2 = new Audio('./audio/owned.mp3');
+
 
     let computerChoices = ['rock', 'paper', 'scissors'];
 
@@ -14,38 +20,24 @@ document.addEventListener('DOMContentLoaded', () => {
     let startGameForm = document.querySelector('.start-game-form');
     let userChoiceScreen = document.querySelector('.user-choice-screen');
 
-    let userRockImage = document.querySelector('.user-rock').childNodes[1];
-    let userPaperImage = document.querySelector('.user-paper').childNodes[1];
-    let userScissorImage = document.querySelector('.user-scissors').childNodes[1];
-    let compRockImage = document.querySelector('.comp-rock').childNodes[1];
-    let compPaperImage = document.querySelector('.comp-paper').childNodes[1];
-    let compScissorsImage = document.querySelector('.comp-scissors').childNodes[1];
+    let userRockImage = document.querySelector('.user-rock').childNodes[1].cloneNode(true);
+    let userPaperImage = document.querySelector('.user-paper').childNodes[1].cloneNode(true);
+    let userScissorImage = document.querySelector('.user-scissors').childNodes[1].cloneNode(true);
+    let compRockImage = document.querySelector('.comp-rock').childNodes[1].cloneNode(true);
+    let compPaperImage = document.querySelector('.comp-paper').childNodes[1].cloneNode(true);
+    let compScissorsImage = document.querySelector('.comp-scissors').childNodes[1].cloneNode(true);
     let compResultContainer = document.querySelector('.comp-result-image-container');
     let userResultContainer = document.querySelector('.user-result-image-container');
 
-    // RESET GAME
-    function resetGame() {
-        userChoice = null;
-        compChoice = null;
-        winner = null;
-    }
+    let scoreboard = document.querySelector('.scoreboard');
 
-    // GET PLAYER NAME AND REVEAL USER CHOICE SCREEN
-    startGameForm.addEventListener('submit', function(e) {
-        // keep form from doing normal shit
-        e.preventDefault();
-        var inputValue = document.querySelector('#name').value
-        // set player name variable
-        playerName = inputValue;
-        // animate splash screen up
-        let splashScreen = document.querySelector('.splash-screen');
-        splashScreen.style.marginTop = '-100vh';
-    })
+    let winningText = document.querySelector('#winning-text');
 
     // SET EVENT LISTENERS ON USER CHOICES
     rock.addEventListener('click', () => {
         userChoice = userChoice == null || undefined ?  "rock" : userChoice
         // Call function to get computer choice
+        console.log(userChoice);
         getComputerChoice();
     });
 
@@ -63,11 +55,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Function to get computer choice from computer choice array
     function getComputerChoice() {
+        console.log(compChoice);
+
         if(compChoice == null || undefined) {
             compChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            console.log(`The user has chosen: ${userChoice}`);
-            console.log(`The computer has chosen: ${compChoice}!`);
-            // Call Winner Function
+            console.log(compChoice);
+        // Call Winner Function
             findWinner();
         }
     }
@@ -81,25 +74,21 @@ document.addEventListener('DOMContentLoaded', () => {
             switch(userChoice) {
                 case 'rock':
                     winner = compChoice == 'scissors' ? "user" : "comp";
-                    console.log(winner);
                     userChoiceScreen.style.display = "none";
                     displayResults();
                 break;
                 case 'scissors':
                     winner = compChoice == 'rock' ? "comp" : "user";
-                    console.log(winner);
                     userChoiceScreen.style.display = "none";
                     displayResults();
                 break;
                 case 'paper':
                     winner = compChoice == 'rock' ? "user" : "comp";
-                    console.log(winner);
                     userChoiceScreen.style.display = "none";
                     displayResults();
                 break;
             }
         } else {
-            console.log("Tie!");
             userChoiceScreen.style.display = "none";
             displayResults();
         }
@@ -108,8 +97,9 @@ document.addEventListener('DOMContentLoaded', () => {
     function displayResults() {
         userResultContainer.appendChild(userRockImage);
         compResultContainer.appendChild(compRockImage);
-
+        paperAudio.play();
         setTimeout(function() {
+            
             userResultContainer.innerHTML = "";
             compResultContainer.innerHTML = "";
             switch(userChoice) {
@@ -145,17 +135,58 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function showWinner() {
-        let winningText = document.querySelector('#winning-text');
+        
         if(winner == 'user') {
+            let winningOptions = [winAudio, winAudio2];
+            let sound = winningOptions[Math.floor(Math.random() * winningOptions.length)];
+            sound.play();
             winningText.innerHTML = "You Win!"
             compResultContainer.style.backgroundColor = "#d63031";
             userResultContainer.style.backgroundColor = "#00b894";
             winningText.style.color = "#00b894";
+            playerScore ++;
+            scoreboard.innerHTML = `${playerScore} - ${compScore}`;
         } else if(winner == 'comp') {
+            loseAudio.play();
             winningText.innerHTML = "You Lose!"
+            compResultContainer.style.backgroundColor = "#00b894";
+            userResultContainer.style.backgroundColor = "#d63031";
+            winningText.style.color = "#d63031";
+            compScore++;
+            scoreboard.innerHTML = `${playerScore} - ${compScore}`;
         } else {
             winningText.innerHTML = "You Tied!"
+            compResultContainer.style.backgroundColor = "#74b9ff";
+            userResultContainer.style.backgroundColor = "#74b9ff";
+            winningText.style.color = "#74b9ff";
         }
+        playAgain();
+    }
+
+    function playAgain() {
+        let playAgainContainer = document.querySelector('.play-again-container');
+        playAgainContainer.style.display = "block";
+        let playAgainButton = document.querySelector('.play-again');
+        playAgainButton.addEventListener('click', function() {
+            resetGame();
+        });
+    }
+
+    function resetGame() {
+        let images = [userPaperImage, userRockImage, userScissorImage, compPaperImage, compRockImage, compScissorsImage]
+        images.forEach((image) => {
+            image.style.animation = "bounceUp 0.5s infinite";
+        });
+        winningText.innerHTML = "The result is...";
+        winningText.style.color = "black";
+        userResultContainer.innerHTML = "";
+        compResultContainer.innerHTML = "";
+        userChoiceScreen.style.display = "flex";
+        compResultContainer.style.backgroundColor = "#fff";
+        userResultContainer.style.backgroundColor = "#fff";
+        userChoice = null;
+        compChoice = null;
+        winner = null;
     }
 
 
